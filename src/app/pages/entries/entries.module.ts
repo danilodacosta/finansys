28269da4c7a6da74aa8984a0a990b0c2d796e327
danilodacosta <danilo-gc@hotmail.com>
2019-07-01 18:22:48 -0300

    finalizando formulario de lancamentos

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,14 +1,21 @@
+import { EntryFormComponent } from './entry-form/entry-form.component';
 import { EntryListComponent } from './entry-list/entry-list.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { EntriesRoutingModule } from './entries-routing.module';
 
+import {CalendarModule} from 'primeng/calendar';
+import {IMaskModule} from 'angular-imask';
+
 @NgModule({
-  declarations: [EntryListComponent],
+  declarations: [EntryListComponent , EntryFormComponent],
   imports: [
     CommonModule,
-    EntriesRoutingModule
+    EntriesRoutingModule,
+    ReactiveFormsModule,
+    CalendarModule,
+    IMaskModule
   ]
 })
 export class EntriesModule { }
