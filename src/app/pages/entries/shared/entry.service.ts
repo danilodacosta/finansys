3fc6ff01850eab2a818908650df36b5740e5fb7f
danilodacosta <danilo-gc@hotmail.com>
2019-07-04 18:59:20 -0300

    criando baseFormResource

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,4 +1,4 @@
-import { flatMap } from 'rxjs/operators';
+import { flatMap, catchError } from 'rxjs/operators';
 import { Injectable, Injector } from '@angular/core';
 
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
@@ -16,26 +16,20 @@ export class EntryService extends BaseResourceService<Entry> {
   }
 
   create(entry: Entry): Observable<Entry> {
-    // entry.categoryId = 1 => moradia
-    // entry.category = category
-    // Observable<Resolvido> => entry
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap(category => {
-        entry.category = category;
-
-        return super.create(entry);
-      })
-    );
+    return this.setCategoryAndSendToServer(entry , super.create.bind(this));
   }
 
   update(entry: Entry): Observable<Entry> {
+    return this.setCategoryAndSendToServer(entry , super.update.bind(this));
+  }
 
+  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap(category => {
         entry.category = category;
-
-        return super.update(entry);
-      })
+        return sendFn(entry);
+      }),
+      catchError(this.handlerError)
     );
   }
 }
