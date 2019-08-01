7ee465d78b4029df1668babf4e8ee4d2d5f5795c
danilodacosta <danilo-gc@hotmail.com>
2019-08-01 01:56:36 -0300

    extraindo e utilizando componente form-field-error

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -20,7 +20,7 @@ export class CategoryFormComponent extends BaseResourceFormComponent<Category> {
   protected buildResourceForm(): void {
      this.resourceForm = this.formBuilder.group({
       id : [null],
-      name: [null, [Validators.required, Validators.minLength(2)]],
+      name: [null, [Validators.required, Validators.minLength(4)]],
       description: [null]
     });
   }
